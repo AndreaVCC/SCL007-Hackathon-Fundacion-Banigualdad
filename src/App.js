a8eea203aba1499ab7e9888f7b575fc393d29436
Andrea Carreño <45242344+AndreaVCC@users.noreply.github.com>
2019-03-21 15:47:28 -0300

    pagina estatica, trayendo solo nombre de la base de datos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Proveedores from './components/Proveedores'
+import Emprendedores from './components/Emprendedores'
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className ="App">
         
-        <Proveedores/>
+        <Emprendedores/>
 
       </div>
     );
